bd68e54a3564df8c2cebf141b9642e16c96cba91
zigzag1001 <72932714+zigzag1001@users.noreply.github.com>
2023-04-22 00:09:23 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image/stb_image_write.h"
+
+int main(void) {
+    int width, height, channels;
+    unsigned char *img = stbi_load("img.jpg", &width, &height, &channels, 0);
+    if (img == NULL) {
+        printf("Error in loading the image\n");
+        exit(1);
+    }
+    printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
+
+    stbi_write_png("img.png", width, height, channels, img, width * channels);
+
+    stbi_image_free(img);
+}
