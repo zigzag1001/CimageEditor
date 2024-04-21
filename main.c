62197fff40dfd5baea9905e1191d079888780c1b
zigzag1001 <peter.saburov@gmail.com>
2024-04-21 02:48:07 -0400

    multiple modes, direction choosing

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <getopt.h>
+#include <stdbool.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
@@ -45,6 +46,9 @@ int getIndex(int x, int y) {
 }
 
 int getPxlBrightness(uint8_t* img, int x, int y) {
+    // if (img_c == 4 && img[(y * img_w + x) * img_c + 3] == 0) {
+    //     return 255;
+    // }
     int sum = 0;
     for (int c = 0; c < 3; c++) {
         sum += img[(y * img_w + x) * img_c + c];
@@ -67,7 +71,11 @@ void swapPxl(uint8_t* img, int x1, int y1, int x2, int y2) {
     }
 }
 
-void test(uint8_t* img, int randOffset, int randChance, int tolerance) {
+bool isOutOfBounds(int x, int y) {
+    return x < 0 || x >= img_w || y < 0 || y >= img_h;
+}
+
+void pxlBleed(uint8_t* img, int randOffset, int randChance, int tolerance, int xy_mode) {
     int dist, brightness;
     uint8_t* orig_img = malloc(img_w * img_h * img_c);
     memcpy(orig_img, img, img_w * img_h * img_c);
@@ -76,9 +84,34 @@ void test(uint8_t* img, int randOffset, int randChance, int tolerance) {
             brightness = getPxlBrightness(img, x, y);
             if (brightness < tolerance || rand() % 100 < randChance) {
                 dist = rand() % randOffset;
-                if (y * img_w + x + dist < img_h * img_w) {
-                    copyPxl(img, x, y, x + dist, y);
-                    // swapPxl(img, x, y, x + dist, y);
+                if (xy_mode == 0) {
+                    copyPxl(img, x, y, (x + dist) % img_w, y);
+                } else if (xy_mode == 1) {
+                    copyPxl(img, x, y, x, (y + dist) % img_h);
+                } else {
+                    copyPxl(img, x, y, (x + dist) % img_w, (y + dist) % img_h);
+                }
+            }
+        }
+    }
+    free(orig_img);
+}
+
+void pxlDiffuse(uint8_t* img, int randOffset, int randChance, int tolerance, int xy_mode) {
+    int dist, brightness;
+    uint8_t* orig_img = malloc(img_w * img_h * img_c);
+    memcpy(orig_img, img, img_w * img_h * img_c);
+    for (int y = 0; y < img_h; y++) {
+        for (int x = 0; x < img_w; x++) {
+            brightness = getPxlBrightness(img, x, y);
+            if (brightness < tolerance || rand() % 100 < randChance) {
+                dist = rand() % randOffset;
+                if (xy_mode == 0) {
+                    swapPxl(img, x, y, (x + dist) % img_w, y);
+                } else if (xy_mode == 1) {
+                    swapPxl(img, x, y, x, (y + dist) % img_h);
+                } else {
+                    swapPxl(img, x, y, (x + dist) % img_w, (y + dist) % img_h);
                 }
             }
         }
@@ -96,10 +129,11 @@ int main(int argc, char** argv) {
         int tol;
         int frame_rate;
         int randChance;
-        int help;
-    } arguments = {50, 2, 128, 20, 10};
+        char *mode;
+        int xy_mode;
+    } arguments = {50, 2, 128, 20, 10, NULL, 2};
 
-    while ((opt = getopt(argc, argv, "i:o:t:f:r:h")) != -1) {
+    while ((opt = getopt(argc, argv, "i:o:t:f:r:m:hxy")) != -1) {
         switch (opt) {
             case 'i':
                 arguments.iterations = atoi(optarg);
@@ -116,18 +150,34 @@ int main(int argc, char** argv) {
             case 'r':
                 arguments.randChance = atoi(optarg);
                 break;
-            case 'h':
-                arguments.help = 1;
+            case 'm':
+                arguments.mode = optarg;
                 break;
+            case 'x':
+                arguments.xy_mode = 0;
+                break;
+            case 'y':
+                arguments.xy_mode = 1;
+                break;
+            case 'h':
+                printf("Usage: %s [-i iterations] [-o offset] [-t tolerance] [-f frame_rate] [-r randChance] [-m mode] [-xy] image\n", argv[0]);
+                printf("Options:\n"
+                        "  -i iterations: Number of frames to generate (default 50)\n"
+                        "  -o offset: Maximum pixel offset (default 2)\n"
+                        "  -t tolerance: Minimum brightness to trigger effect (default 128)\n"
+                        "  -f frame_rate: Frame rate of output gif (default 20)\n"
+                        "  -r randChance: Chance of effect happening (default 10)\n"
+                        "  -m mode: Effect mode bleed/diffuse/wind/haze (default bleed)\n"
+                        "  -x: Only offset x axis\n"
+                        "  -y: Only offset y axis\n"
+                        "  -h: Show help\n"
+                        "  image: Path to image\n"
+                );
+                exit(0);
             default:
-                fprintf(stderr, "Usage: %s [-i iterations] [-o offset] [-t tolerance] [-f frame_rate] [-r randChance] image\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-i iterations] [-o offset] [-t tolerance] [-f frame_rate] [-r randChance] [-m mode] [-xy] image\n", argv[0]);
                 exit(1);
         }
-    }
-
-    if (arguments.help) {
-        printf("Usage: %s [-i iterations] [-o offset] [-t tolerance] [-f frame_rate] [-r randChance] image\n", argv[0]);
-        exit(0);
     }
 
     if (optind >= argc) {
@@ -138,14 +188,22 @@ int main(int argc, char** argv) {
     loadImg(argv[optind], &img_w, &img_h, &img_c);
 
     printf("Loaded image with dimensions %dx%d and %d channels\n", img_w, img_h, img_c);
-    printf("Total pixels: %d\n", img_w * img_h);
 
     uint8_t* mod_img =  malloc(img_w * img_h * img_c);
     memcpy(mod_img, og_img, img_w * img_h * img_c);
+
+    char* progress = "Processing frame %d/%d";
+
     for (int i = 0; i < arguments.iterations; i++) {
-        test(mod_img, arguments.offset, arguments.randChance, arguments.tol);
+        if (arguments.mode == NULL || strcmp(arguments.mode, "bleed") == 0 || strcmp(arguments.mode, "wind") == 0)
+            pxlBleed(mod_img, arguments.offset, arguments.randChance, arguments.tol, arguments.xy_mode);
+        else if (strcmp(arguments.mode, "diffuse") == 0 || strcmp(arguments.mode, "haze") == 0)
+            pxlDiffuse(mod_img, arguments.offset, arguments.randChance, arguments.tol, arguments.xy_mode);
         srand(i);
-        printf("Test %d\n", i);
+
+        printf("\r");
+        printf(progress, i, arguments.iterations);
+
         char filename[50];
         strcpy(filename, "./img/");
         char num[5];
@@ -153,6 +211,9 @@ int main(int argc, char** argv) {
         strcat(filename, num);
         strcat(filename, ".png");
         saveImg(filename, img_w, img_h, img_c, mod_img);
+
+        if (arguments.mode != NULL && (strcmp(arguments.mode, "wind") == 0 || strcmp(arguments.mode, "haze") == 0))
+            memcpy(mod_img, og_img, img_w * img_h * img_c);
     }
     stbi_image_free(og_img);
 
